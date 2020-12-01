515c78a39bee62ddaa2cc2ec48cca8d5b45e701d
chisato <kitada.c08@gmail.com>
2020-12-01 11:20:38 +0900

    footer作成 #2

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-footer
+    dark
+    color = "#8bc34a"
+    absolute
+  >
+    <div class="footer">
+      Copyright 2020. chisato kitada
+    </div>
+  </v-footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+  .footer {
+    margin: 0 auto;
+  }
+</style>
