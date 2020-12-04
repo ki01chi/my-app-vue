c2e348242abacf64e97bafa853d5abb247217e2a
chisato <kitada.c08@gmail.com>
2020-12-04 23:52:31 +0900

    firebaseとregister連携

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,15 +4,41 @@
     <form class="login-form">
       <div class="input-group">
         <label for="email">メールアドレス</label>
-        <input type="email" id="email">
+        <input type="email" id="email" v-model="email" />
       </div>
       <div class="input-group">
         <label for="password">パスワード</label>
-        <input type="password" id="password">
+        <input type="password" id="password" v-model="password" />
       </div>
       <div class="input-group">
-        <button type="button">新規登録</button>
+        <button type="button" @click="register()">新規登録</button>
       </div>
     </form>
   </div>
 </template>
+
+<script>
+import firebase from "firebase";
+
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    register() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(
+          alert("新規登録完了")
+        )
+        .catch((error) => {
+          alert(error.message);
+        });
+    },
+  },
+};
+</script>
